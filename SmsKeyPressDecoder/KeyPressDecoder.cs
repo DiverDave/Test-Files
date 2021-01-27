b9ef8dd1fb3f541b3407ae1e36532fee0deb70ac
Dave <dave.kerr@aspeq.com>
2021-01-27 23:10:45 +1300

    add back working solution

--- SmsKeyPressDecoder/KeyPressDecoder.cs
+++ SmsKeyPressDecoder/KeyPressDecoder.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 
 namespace SmsKeyPressDecoder
 {
@@ -8,9 +6,21 @@ namespace SmsKeyPressDecoder
     {
         public KeyPressDecoder()
         {
-           
+            this.KeyPressDictionary = new Dictionary<string, string>
+            {
+                {"2", "ABC"},
+                {"3", "DEF"},
+                {"4", "GHI"},
+                {"5", "JKL"},
+                {"6", "MNO"},
+                {"7", "PQRS"},
+                {"8", "TUV"},
+                {"9", "WXYZ"},
+                {"0", " "}
+            };
         }
 
+        Dictionary<string, string> KeyPressDictionary { get; }
 
         /// <summary>
         /// Decodes a string representing key presses on a multi tap feature phone. See read me for more information.
@@ -20,12 +30,52 @@ namespace SmsKeyPressDecoder
         public string Decode(string inputKeys)
         {
             string message = null;
+            var index = 0;
+            var lastKey = "";
+            while (index < inputKeys.Length)
+            {
+                var keyChar = inputKeys[index];
+                var keyCount = 1;
+                while (index < inputKeys.Length - 1 && keyChar == inputKeys[index + 1])
+                {
+                    keyCount++;
+                    index++;
+                }
+
+                var key = keyChar.ToString();
+                if (key == "0")
+                {
+                    message += DecodeZero(inputKeys, index, keyCount, lastKey);
+                }
+                else
+                {
+                    var keyIndex = (keyCount - 1) % this.KeyPressDictionary[key].Length;
+                    message += this.KeyPressDictionary[key][keyIndex];
+                    lastKey = key;
+                }
+
+                index++;
+            }
 
             // add your code here
             // create as many unit tests or other functions as you like
-
+            // tests are not case sensitive
+            ///.....
             return message;
         }
+
+        private string DecodeZero(string inputKeys, in int index, int keyCount, string lastKey)
+        {
+            string nextKey = inputKeys.Length > index + 1 ? inputKeys[index + 1].ToString() : null;
+            var spaceCount = keyCount;
+            if (nextKey == lastKey)
+            {
+                spaceCount -= 1;
+            }
+
+            return string.Empty.PadLeft(spaceCount, ' ');
+        }
+
 
     }
 }
